a5b22c49df992b1a42815da7201db7c3669e0ca5
Jan Hromádka <jan.hromadka@ydealsoft.com>
2021-11-09 11:43:58 +0100

    updated header

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <nav>
+    <!-- <nav>
       <div>
         <img width="140" height="140" alt="logo" title="logo" src="../img/teskab_final_png.png">
       </div>
@@ -22,9 +22,32 @@
         </div>
       </div>
       <div><a class="hamburger" onclick="openNav()">☰</a></div>
-    </nav>
+    </nav> -->
 
     <div class="header__main">
+      <nav>
+        <!-- <div>
+          <img width="140" height="140" alt="logo" title="logo" src="../img/teskab_final_png.png">
+        </div> -->
+
+        <ul>
+          <li><a href="index.html">Úvod</a></li>
+          <li><a href="services.html">Služby</a></li>
+          <li><a href="gallery.html">Galerie</a></li>
+          <li><a href="contact.html">Kontakt</a></li>
+        </ul>
+
+        <div id="myNav" class="overlay">
+          <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+          <div class="overlay-content">
+            <a href="index.html">Úvod</a>
+            <a href="services.html">Služby</a>
+            <a href="gallery.html">Galerie</a>
+            <a href="contact.html">Kontakt</a>
+          </div>
+        </div>
+        <div><a class="hamburger" onclick="openNav()">☰</a></div>
+      </nav>
       <div class="header__main--title">
         <h1 class="main">
           Tesařství kabeláč
